dd674667e2ad70bd0ff8718b2df526f15b80f4ab
Ali Farid <alifarid0011@gmail.com>
2025-09-26 05:19:08 +0330

    feat(repository): Add Casbin AttributeRepository interface and implementation

---
+++ internal/controllers/user_controller.go
@@ -0,0 +1,13 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+type UserController interface {
+	FindByUsername(ctx *gin.Context)
+	FindUserByUID(ctx *gin.Context)
+	CreateUser(ctx *gin.Context)
+	GetAllUsers(ctx *gin.Context)
+	UpdateUser(ctx *gin.Context)
+	DeleteUser(ctx *gin.Context)
+	Me(ctx *gin.Context)
+}
