e0f087a2b9b81d65d3b1c48ac430e541c2f99f84
Ali Farid <alifarid0011@gmail.com>
2025-09-26 21:55:12 +0330

    feat(middleware): add auth auth middleware

---
+++ internal/middleware/auth_middleware.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"github.com/Alifarid0011/questionnaire-back-end/internal/dto/response"
+	"github.com/Alifarid0011/questionnaire-back-end/internal/repository"
+	"github.com/Alifarid0011/questionnaire-back-end/utils"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func AuthMiddleware(blackRepo repository.BlackListTokenRepository, tokenManager utils.JwtToken) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		tokenString, err := utils.ExtractBearerToken(authHeader)
+		if err != nil {
+			response.New(c).Status(http.StatusUnauthorized).Errors(err).Message("invalid token").MessageID("auth.middleware.failed").Dispatch()
+			return
+		}
+		token, err := tokenManager.ParseToken(tokenString)
+		if err != nil {
+			response.New(c).Status(http.StatusUnauthorized).Errors(err).Message("invalid token").MessageID("auth.middleware.failed").Dispatch()
+			return
+		}
+		blackToken, errBlackRepo := blackRepo.FindByToken(tokenString)
+		if blackToken != nil && errBlackRepo != nil {
+			response.New(c).Status(http.StatusUnauthorized).Errors(err).Message("token is in the black list !").MessageID("auth.middleware.black_list").Dispatch()
+			return
+		}
+		c.Set("claims", token)
+		c.Set("user_uid", token.UID)
+		c.Set("access_token", tokenString)
+		c.Next()
+	}
+}
