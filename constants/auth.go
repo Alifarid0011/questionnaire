cf50ec03fbbc0e60b1de76c2481e2aa95d3dc900
Ali Farid <alifarid0011@gmail.com>
2025-09-26 05:36:11 +0330

    chore(constant): add auth constant to project

---
+++ constants/auth.go
@@ -0,0 +1,16 @@
+package constants
+
+type TokenType = string
+
+const (
+	AccessToken  TokenType = "access"
+	RefreshToken TokenType = "refresh"
+)
+
+const (
+	RefreshTokenType TokenType = "refresh_token"
+	AccessTokenType  TokenType = "access_token"
+	UserUid          TokenType = "user_uid"
+)
+
+const ContextRolesKey = "roles"
