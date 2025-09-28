770aa677762072ed1ba8635e53a06017cb1228b6
ali <alifarid0011@gmail.com>
2025-09-28 19:44:09 +0330

    fix(dto): fix name of user answer dto

--- internal/dto/user_answer_dto.go
+++ internal/dto/user_answer_dto.go
@@ -2,14 +2,8 @@ package dto
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type CreateUserAnswerDTO struct {
+type UserAnswerDTO struct {
 	QuizID  primitive.ObjectID     `json:"quiz_id" binding:"required"`
 	UserID  primitive.ObjectID     `json:"user_id" binding:"required"`
 	Answers map[string]interface{} `json:"answers" binding:"required"`
-}
-
-type UpdateUserAnswerDTO struct {
-	ID      primitive.ObjectID     `json:"id" binding:"required"`
-	Answers map[string]interface{} `json:"answers,omitempty"`
-	Score   *float64               `json:"score,omitempty"`
 }
